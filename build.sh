65534c19cf1a6a0caeba34771f3222c16a011e40
Samuel Fisher <SamuelFisher@users.noreply.github.com>
2024-03-24 21:37:00 +0000

    Build script single shader (#6)

--- build.sh
+++ build.sh
@@ -5,17 +5,33 @@ mkdir -p build
 
 src_dir="./src"
 
-for filename in "$src_dir"/*/shader.py; do
-  # Check it's a regular file (not a directory or hidden file)
-  if [[ -f "$filename" ]]; then
-    shader_name=$(dirname "$filename")
-    shader_name=${shader_name##*/}
+# Builds a single shader
+build_shader() {
+  local shader_name="$1"
+  local filename="$src_dir/$shader_name/shader.py"
+
+  if [[ -f "$filename" ]]; then  # Make sure it's a regular file
     echo "Building $shader_name"
 
     componentize-py -d wit/circuit-shader.wit -w circuit-shader componentize -p src/$shader_name shader -o build/$shader_name.wasm
     if [[ $? -ne 0 ]]; then
       echo "Error: failed to build $shader_name"
-      exit 1 
+      exit 1
     fi
+  else
+    echo "Error: shader $shader_name not found"
+    exit 1
   fi
-done
+}
+
+if [[ $# -eq 0 ]]; then  # If no argument is given, build all shaders
+  for filename in "$src_dir"/*/shader.py; do
+    if [[ -f "$filename" ]]; then
+      shader_name=$(dirname "$filename")
+      shader_name=${shader_name##*/}
+      build_shader "$shader_name"
+    fi
+  done
+else # If an argument is given, build only the specified shader
+  build_shader "$1"
+fi
